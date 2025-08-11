89da018b2007505fbfd0776504b69d2cb115d0a8
Patrick Heese <pheese8@gmail.com>
2025-08-11 18:19:56 -0400

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name for storing images"
+  type        = string
+}
