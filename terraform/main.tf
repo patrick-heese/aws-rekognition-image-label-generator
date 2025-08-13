eed8112ad45f30d979cf8885aabaa3526503d026
Patrick Heese <pheese8@gmail.com>
2025-08-13 00:30:37 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,3 @@ resource "aws_s3_bucket" "image_bucket" {
     Environment = "Dev"
   }
 }
-
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.image_bucket.bucket
-}
