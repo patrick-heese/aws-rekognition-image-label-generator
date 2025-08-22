e1484eb6dcb65df044a5ba34b290aebb9ff1e8ed
Patrick Heese <pheese8@gmail.com>
2025-08-21 22:47:31 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_s3_bucket" "image_bucket" {
   bucket = var.bucket_name
 
